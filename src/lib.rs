f72d83903403b1195c9966bddb956155e9293acf
Amirabbas Fazelinia <utilyre@proton.me>
2023-08-03 11:41:30 +0330

    feat(orientation): implement mouse rotation capability

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,18 @@
-use bevy::prelude::*;
+use bevy::{input::mouse::MouseMotion, prelude::*};
+use std::f32::consts;
 
 pub mod prelude;
 
 pub struct PrankPlugin {
     default_direction_input: bool,
+    default_orientation_input: bool,
 }
 
 impl Default for PrankPlugin {
     fn default() -> Self {
         Self {
             default_direction_input: true,
+            default_orientation_input: true,
         }
     }
 }
@@ -18,13 +21,18 @@ impl Plugin for PrankPlugin {
     fn build(&self, app: &mut App) {
         app.add_event::<Prank3dAbsoluteDirection>();
         app.add_event::<Prank3dRelativeDirection>();
+        app.add_event::<Prank3dRotation>();
         app.register_type::<Prank3d>();
 
         if self.default_direction_input {
             app.add_systems(PreUpdate, direction_input);
         }
+        if self.default_orientation_input {
+            app.add_systems(PreUpdate, rotation_input);
+        }
 
         app.add_systems(Update, movement);
+        app.add_systems(Update, orientation);
     }
 }
 
@@ -81,15 +89,34 @@ fn direction_input(
     rdir_event.send(Prank3dRelativeDirection(rdir));
 }
 
+#[derive(Event)]
+pub struct Prank3dRotation(pub Vec2);
+
+fn rotation_input(
+    mut rotation_event: EventWriter<Prank3dRotation>,
+    mut mouse_event: EventReader<MouseMotion>,
+) {
+    let rotation = mouse_event.iter().fold(Vec2::ZERO, |acc, x| acc + x.delta);
+    rotation_event.send(Prank3dRotation(rotation));
+}
+
 #[derive(Reflect, Component)]
 #[reflect(Component)]
 pub struct Prank3d {
     pub speed: f32,
+    pub sensitivity: Vec2,
+    pub pitch: f32,
+    pub yaw: f32,
 }
 
 impl Default for Prank3d {
     fn default() -> Self {
-        Self { speed: 5.0 }
+        Self {
+            speed: 5.0,
+            sensitivity: Vec2::splat(0.08),
+            pitch: 0.0,
+            yaw: 0.0,
+        }
     }
 }
 
@@ -110,5 +137,21 @@ fn movement(
         let rdir = transform.rotation * rdir;
         transform.translation +=
             prank.speed * (adir + rdir).normalize_or_zero() * time.delta_seconds();
+    }
+}
+
+fn orientation(
+    mut pranks: Query<(&mut Transform, &mut Prank3d)>,
+    mut rotation: EventReader<Prank3dRotation>,
+    time: Res<Time>,
+) {
+    let rotation = rotation.iter().fold(Vec2::ZERO, |acc, x| acc + x.0);
+
+    for (mut transform, mut prank) in pranks.iter_mut() {
+        prank.pitch = (prank.pitch - prank.sensitivity.y * rotation.y * time.delta_seconds())
+            .clamp(-consts::FRAC_PI_2, consts::FRAC_PI_2);
+        prank.yaw -= prank.sensitivity.x * rotation.x * time.delta_seconds();
+
+        transform.rotation = Quat::from_euler(EulerRot::YXZ, prank.yaw, prank.pitch, 0.0);
     }
 }
