070747adc7f2697321e383d674c29aaff10d583a
Amirabbas Fazelinia <utilyre@proton.me>
2023-08-19 13:50:27 +0330

    docs(prelude): rephrase the module doc

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,4 +1,4 @@
-//! `use bevy_prank::prelude::*` to import common resources, components, etc.
+//! Re-exports common resources, components, etc.
 
 pub use crate::{
     three::{gizmo::Prank3dGizmoConfig, hud::Prank3dHudConfig, Prank3d},
